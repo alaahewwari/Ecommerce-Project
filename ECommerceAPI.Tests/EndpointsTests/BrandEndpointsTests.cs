4e759c9715d21809acec02749ddb420e4b5eda27
Alaa Hewwari <alaahewwari@gmail.com>
2024-05-27 01:54:57 +0300

    Refactor BrandEndpointTests for clarity and consistency

--- ECommerceAPI.Tests/EndpointsTests/BrandEndpointsTests.cs
+++ ECommerceAPI.Tests/EndpointsTests/BrandEndpointsTests.cs
@@ -4,13 +4,12 @@ using ECommerceAPI.Dtos.Product.Responses;
 using ECommerceAPI.Endpoints;
 using ECommerceAPI.ErrorHandling;
 using ECommerceAPI.Services.BrandServices.Interfaces;
-using ECommerceAPI.Services.ProductServices.Interfaces;
 using ErrorOr;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 
-namespace ECommerceAPI.Tests.Endpoints
+namespace ECommerceAPI.Tests.EndpointsTests
 {
     public class BrandEndpointsTests
     {
@@ -23,66 +22,65 @@ namespace ECommerceAPI.Tests.Endpoints
         }
 
         [Fact]
-        public async Task GetBrands_ShouldReturnOkResult_WithListOfBrands()
+        public async Task GetBrands_ShouldReturnSuccess_WithListOfBrands()
         {
-            var brands = _fixture.CreateMany<BrandResponseDto>(10).ToList();
-            _mockBrandService.Setup(service => service.GetBrandsAsync()).ReturnsAsync(brands);
+            var expected = _fixture.CreateMany<BrandResponseDto>(10).ToList();
+
+            _mockBrandService.Setup(service => service.GetBrandsAsync()).ReturnsAsync(expected);
 
             var result = await BrandEndpoints.GetBrands(_mockBrandService.Object);
 
-            var okResult = result.Should().BeOfType<Ok<IEnumerable<BrandResponseDto>>>().Subject;
-            okResult.Value.Should().BeEquivalentTo(brands);
+            var actual = result.Should().BeOfType<Ok<IEnumerable<BrandResponseDto>>>().Subject;
+            actual.Value.Should().BeEquivalentTo(expected);
         }
         [Fact]
         public async Task GetBrandById_ShouldReturnOkResult_WhenBrandFound()
         {
-            var brand = _fixture.Create<BrandResponseDto>();
-            ErrorOr<BrandResponseDto> success = brand;
+            var expected = _fixture.Create<BrandResponseDto>();
+            ErrorOr<BrandResponseDto> success = expected;
             _mockBrandService.Setup(service => service.GetBrandByIdAsync(It.IsAny<long>())).ReturnsAsync(success);
 
             var result = await BrandEndpoints.GetBrandById(_mockBrandService.Object, 1);
 
-            var okResult = result.Should().BeOfType<Ok<BrandResponseDto>>().Subject;
-            okResult.Value.Should().BeEquivalentTo(brand);
+            var actual = result.Should().BeOfType<Ok<BrandResponseDto>>().Subject;
+            actual.Value.Should().BeEquivalentTo(expected);
         }
         [Fact]
-        public async Task GetCategoryById_ShouldReturnNotFound_WhenCategoryNotFound()
+        public async Task GetBrandyById_ShouldReturnNotFound_WhenBrandNotFound()
         {
-            var error = CategoryErrors.CategoryNotFound;
-            _mockBrandService.Setup(service => service.GetBrandByIdAsync(It.IsAny<long>())).ReturnsAsync(error);
+            var expected = CategoryErrors.CategoryNotFound;
+            _mockBrandService.Setup(service => service.GetBrandByIdAsync(It.IsAny<long>())).ReturnsAsync(expected);
 
             var result = await BrandEndpoints.GetBrandById(_mockBrandService.Object, 1);
 
-            var notFoundResult = result.Should().BeOfType<NotFound<List<Error>>>().Subject;
-            notFoundResult.Value.Should().Contain(error);
-        }
-
-        [Fact]
-        public async Task CreateBrand_ShouldReturnBadRequest_WhenBrandNotCreated()
-        {
-            var createBrandRequest = _fixture.Create<CreateBrandRequestDto>();
-            var error = BrandErrors.BrandCreationFailed;
-            _mockBrandService.Setup(service => service.CreateBrandAsync(createBrandRequest)).ReturnsAsync(error);
-
-            var result = await BrandEndpoints.CreateBrand(_mockBrandService.Object, createBrandRequest);
-
-            var badRequestResult = result.Should().BeOfType<BadRequest<string>>().Subject;
-            badRequestResult.Value.Should().Be("Brand not created");
+            var actual = result.Should().BeOfType<NotFound<List<Error>>>().Subject;
+            actual.Value.Should().Contain(expected);
         }
 
         [Fact]
         public async Task CreateBrand_ShouldReturnCreatedResult_WithBrand()
         {
-            var createBrandRequest = _fixture.Create<CreateBrandRequestDto>();
-            var brand = _fixture.Create<BrandResponseDto>();
-            ErrorOr<BrandResponseDto> success = brand;
-            _mockBrandService.Setup(service => service.CreateBrandAsync(createBrandRequest)).ReturnsAsync(success);
+            var brands = _fixture.Create<BrandRequestDto>();
+            var expected = _fixture.Create<BrandResponseDto>();
+            ErrorOr<BrandResponseDto> success = expected;
+            _mockBrandService.Setup(service => service.CreateBrandAsync(brands)).ReturnsAsync(success);
+            var result = await BrandEndpoints.CreateBrand(_mockBrandService.Object, brands);
+            var actual = result.Should().BeOfType<Created<BrandResponseDto>>().Subject;
+            actual.Location.Should().Be($"/api/brands/{expected.Id}");
+            actual.Value.Should().BeEquivalentTo(expected);
+        }
+        [Fact]
+        public async Task CreateBrand_ShouldReturnBadRequest_WhenBrandNotCreated()
+        {
+            var expected = BrandErrors.BrandCreationFailed;
+            var createBrandRequest = _fixture.Create<BrandRequestDto>();
+            _mockBrandService.Setup(service => service.CreateBrandAsync(createBrandRequest)).ReturnsAsync(expected);
 
             var result = await BrandEndpoints.CreateBrand(_mockBrandService.Object, createBrandRequest);
 
-            var createdResult = result.Should().BeOfType<Created<BrandResponseDto>>().Subject;
-            createdResult.Location.Should().Be($"/api/brands/{brand.Id}");
-            createdResult.Value.Should().BeEquivalentTo(brand);
+            var actual = result.Should().BeOfType<BadRequest<List<Error>>>().Subject;
+            actual.Value.Should().Contain(expected);
         }
+
     }
 }
