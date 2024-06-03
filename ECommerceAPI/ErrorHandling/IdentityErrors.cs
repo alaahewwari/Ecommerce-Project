71ccd7ed7359c0171622b6daab6361f420f5dc09
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-03 23:37:19 +0300

    Add identity error handling

---
+++ ECommerceAPI/ErrorHandling/IdentityErrors.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+
+namespace ECommerceAPI.ErrorHandling
+{
+    public static class IdentityErrors
+    {
+        public static readonly Error InvalidToken = Error.Validation(
+                       code: "InvalidToken",
+                                  description: "The provided token is invalid.");
+
+        public static readonly Error UserNotFound = Error.NotFound(
+            code: "UserNotFound",
+            description: "User does not exist.");
+
+        public static readonly Error UserAlreadyExists = Error.Conflict(
+            code: "UserAlreadyExists",
+            description: "User already exists.");
+
+        public static readonly Error UserCreationFailed = Error.Failure(
+            code: "UserCreationFailed",
+            description: "Failed to create the user.");
+    }
+}
