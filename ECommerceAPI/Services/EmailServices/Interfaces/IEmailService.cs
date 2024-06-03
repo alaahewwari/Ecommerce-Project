4e096bf3ed51d5b2ba678b978009f43886d67f54
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-03 23:50:13 +0300

    Add email services to send email for registered user to confirm

---
+++ ECommerceAPI/Services/EmailServices/Interfaces/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerceAPI.Services.EmailServices.Interfaces;
+public interface IEmailService
+{
+        Task SendEmailAsync(string to, string subject, string body);
+    Task SendConfirmationEmailAsync(string to, string url);
+}
