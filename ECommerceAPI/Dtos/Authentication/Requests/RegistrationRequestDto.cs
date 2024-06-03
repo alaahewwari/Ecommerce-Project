279a597a99684d56b86a5fbb9ec4050961ac025a
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-03 23:34:22 +0300

    Implement registration request dto

---
+++ ECommerceAPI/Dtos/Authentication/Requests/RegistrationRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.Dtos.Authentication.Requests
+{
+    public class RegistrationRequestDto
+    {
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public string Email { get; set; } 
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
