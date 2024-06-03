3e4374f41401d3816edc102269b84b24910b8e3c
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-03 23:42:21 +0300

    Implement authentication service with two methods (RegisterAsync and ConfirmEmailAsync)

---
+++ ECommerceAPI/Services/AuthenticationServices/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceAPI.Common;
+using ECommerceAPI.Dtos.Authentication.Requests;
+using ErrorOr;
+
+namespace ECommerceAPI.Services.AuthenticationServices.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<ErrorOr<SuccessResponse>> RegisterAsync(RegistrationRequestDto request, string url);
+        Task<ErrorOr<SuccessResponse>> ConfirmEmailAsync(string email, string token);
+    }
+}
