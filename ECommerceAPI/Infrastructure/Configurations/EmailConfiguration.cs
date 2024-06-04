a35e4b5de0a4f6561dff9b4e21c6cb1766b4786e
Alaa Hewwari <97515289+alaahewwari@users.noreply.github.com>
2024-06-04 13:31:29 +0300

    Update EmailConfiguration.cs

--- ECommerceAPI/Infrastructure/Configurations/EmailConfiguration.cs
+++ ECommerceAPI/Infrastructure/Configurations/EmailConfiguration.cs
@@ -2,10 +2,10 @@
 {
     public class EmailConfiguration
     {
-        public string From { get; set; } = null!;
-        public string SmtpServer { get; set; } = null!;
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
         public int Port { get; set; }
-        public string UserName { get; set; } = null!;
-        public string Password { get; set; } = null!;
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
