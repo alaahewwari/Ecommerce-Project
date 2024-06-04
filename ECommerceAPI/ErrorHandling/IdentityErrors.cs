9f8fac96c3753b600d9386af6259ff77725cb916
Alaa Hewwari <97515289+alaahewwari@users.noreply.github.com>
2024-06-04 13:25:36 +0300

    Update IdentityErrors.cs
    Fix spacing

--- ECommerceAPI/ErrorHandling/IdentityErrors.cs
+++ ECommerceAPI/ErrorHandling/IdentityErrors.cs
@@ -1,5 +1,4 @@
 ﻿using ErrorOr;
-
 namespace ECommerceAPI.ErrorHandling
 {
     public static class IdentityErrors
@@ -7,7 +6,7 @@ namespace ECommerceAPI.ErrorHandling
         public static readonly Error InvalidToken = Error.Validation(
                        code: "InvalidToken",
                                   description: "The provided token is invalid.");
-
+        
         public static readonly Error UserNotFound = Error.NotFound(
             code: "UserNotFound",
             description: "User does not exist.");
