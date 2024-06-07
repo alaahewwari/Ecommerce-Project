7deb8bd1dc23bc1689cbec0cc9bf32c0996b7a01
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-07 23:31:32 +0300

    Move HTML templates and images to wwwroot for static file serving

--- ECommerceAPI/Constants/Email/EmailUrlConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace ECommerceAPI.Constants.Email
-{
-    public class EmailUrlConstants
-    {
-        public static readonly string ConfirmationImageUrl = "https://th.bing.com/th/id/OIP.i_nEQw6xesPjDvPsPgqcRwHaHa?rs=1&pid=ImgDetMain";
-    }
-}
