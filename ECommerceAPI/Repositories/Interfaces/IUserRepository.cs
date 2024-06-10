1a18ffd6515b7bac54fe07c8d9211d520ef37342
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-10 13:13:13 +0300

    Implment FindByRefreshTokenAsync method in user repository

--- ECommerceAPI/Repositories/Interfaces/IUserRepository.cs
+++ ECommerceAPI/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace ECommerceAPI.Repositories.Interfaces
+﻿using ECommerceAPI.Data.Models;
+
+namespace ECommerceAPI.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        Task<User?> FindByRefreshTokenAsync(string refreshToken);
     }
 }
