e11c4c2e37d3daf17bdf0de49abc533218f5eace
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-10 13:03:31 +0300

    Implement token service for generate access and refresh token

---
+++ ECommerceAPI/Services/TokenService/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerceAPI.Data.Models;
+using ECommerceAPI.Dtos.Authentication.Responses;
+namespace ECommerceAPI.Services.TokenService.Interfaces
+{
+    public interface ITokenService
+    {
+         public Task<LoginResponseDto> GenerateTokenAsync(User user);
+    }
+}
