c73a9940ebebe2ae90d2ece631fb015608458acb
Alaa Hewwari <alaahewwari@gmail.com>
2024-06-10 13:08:56 +0300

    Add dtos for login service

---
+++ ECommerceAPI/Dtos/Authentication/Responses/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceAPI.Dtos.Authentication.Responses
+{
+    public class LoginResponseDto
+    {
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiryTime { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
