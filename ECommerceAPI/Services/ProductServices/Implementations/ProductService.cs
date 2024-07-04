be312714f9429fe4e80c3c69bb261a50dd79fb14
Alaa Hewwari <alaahewwari@gmail.com>
2024-07-04 18:06:20 +0300

    Update create and update product services to get authenticated user first

--- ECommerceAPI/Services/ProductServices/Implementations/ProductService.cs
+++ ECommerceAPI/Services/ProductServices/Implementations/ProductService.cs
@@ -1,47 +1,58 @@
-﻿
-using AutoMapper;
+﻿using AutoMapper;
 using ECommerceAPI.Common;
 using ECommerceAPI.Data.Models;
 using ECommerceAPI.Dtos.Product.Requests;
 using ECommerceAPI.Dtos.Product.Responses;
 using ECommerceAPI.ErrorHandling;
 using ECommerceAPI.Repositories.Interfaces;
+using ECommerceAPI.Services.AuthenticationServices.Interfaces;
 using ECommerceAPI.Services.ProductServices.Interfaces;
 using ErrorOr;
-
 namespace ECommerceAPI.Services.ProductServices.Implementations
 {
     public class ProductService(
         IMapper mapper,
         IProductRepository productRepository,
         IBrandRepository brandRepository,
-        ICategoryRepository categoryRepository
+        ICategoryRepository categoryRepository,
+        IAuthenticatedUserService authenticatedUserService
         ) : IProductService
     {
         public async Task<ErrorOr<ProductResponseDto>> CreateProductAsync(ProductRequestDto productDto)
         {
-                var brand = await brandRepository.GetBrandByIdAsync(productDto.BrandId);
-                if (brand is null)
-                {
-                    return BrandErrors.BrandNotFound;
-                }
-                var category = await categoryRepository.GetCategoryByIdAsync(productDto.CategoryId);
-                if (category is null)
-                {
-                    return CategoryErrors.CategoryNotFound;
-                }
-                var product = mapper.Map<Product>(productDto);
+            var userId = authenticatedUserService.GetAuthenticatedUserIdAsync();
+            if (userId == 0)
+            {
+                return IdentityErrors.Unauthenticated;
+            }
+            var brand = await brandRepository.GetBrandByIdAsync(productDto.BrandId);
+            if (brand is null)
+            {
+                return BrandErrors.BrandNotFound;
+            }
+            var category = await categoryRepository.GetCategoryByIdAsync(productDto.CategoryId);
+            if (category is null)
+            {
+                return CategoryErrors.CategoryNotFound;
+            }
+            var product = mapper.Map<Product>(productDto);
             product.CreatedDate = DateTime.Now;
+            product.CreatedUserId = userId;
             var result = await productRepository.CreateProductAsync(product);
-                if (result is null)
-                {
-                    return ProductErrors.ProductCreationFailed;
-                }
-                var response =await GetProductByIdAsync(result.Id);
+            if (result is null)
+            {
+                return ProductErrors.ProductCreationFailed;
+            }
+            var response = await GetProductByIdAsync(result.Id);
             return response;
         }
         public async Task<ErrorOr<ProductResponseDto>> UpdateProductAsync(ProductRequestDto productDto, long id)
         {
+            var userId = authenticatedUserService.GetAuthenticatedUserIdAsync();
+            if (userId == 0)
+            {
+                return IdentityErrors.Unauthenticated;
+            }
             var product = await productRepository.GetProductByIdAsync(id);
             if (product is null)
             {
@@ -58,11 +69,12 @@ namespace ECommerceAPI.Services.ProductServices.Implementations
             {
                 return CategoryErrors.CategoryNotFound;
             }
-
             var updatedProduct = mapper.Map<Product>(productDto);
             updatedProduct.Id = id;
+            updatedProduct.UpdatedDate = DateTime.Now;
+            updatedProduct.UpdatedUserId = userId;
             var result = await productRepository.UpdateProductAsync(updatedProduct);
-var response = mapper.Map<ProductResponseDto>(result);
+            var response = mapper.Map<ProductResponseDto>(result);
             if (response is null)
             {
                 return ProductErrors.ProductUpdateFailed;
@@ -71,7 +83,7 @@ var response = mapper.Map<ProductResponseDto>(result);
         }
         public async Task<ErrorOr<ProductResponseDto>> GetProductByIdAsync(long id)
         {
-var product = await productRepository.GetProductByIdAsync(id);
+            var product = await productRepository.GetProductByIdAsync(id);
             if (product is null)
             {
                 return ProductErrors.ProductNotFound;
@@ -99,5 +111,5 @@ var product = await productRepository.GetProductByIdAsync(id);
             }
             return new SuccessResponse("Product deleted successfully");
         }
-        }
+    }
 }
